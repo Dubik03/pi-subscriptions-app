f307ffcacf0f6400f2891a036379e7bcdfcd05e9
Dubik03 <94638982+Dubik03@users.noreply.github.com>
2025-08-21 06:47:35 +0200

    Update [id].tsx

--- pages/service/[id].tsx
+++ pages/service/[id].tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 export default function ServiceDetail() {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://cdn.pi.com/sdk.js'; // Uprav podle skutečné URL Pi SDK
+    script.src = 'https://sdk.minepi.com/pi-sdk.js'; // Uprav podle skutečné URL Pi SDK
     script.async = true;
     script.onload = () => {
       console.log('Pi SDK loaded');
